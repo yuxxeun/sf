67d87f53251ad13d3c6b27775de4c4c528c788a6
yuxxeun <itsyuxxeun@gmail.com>
2024-03-19 13:35:33 +0700

    feat: geo location component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
 "use client"
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import SmoothScrolling from "@/components/core/SmoothScroll";
 import Footer from '@/components/common/Footer';
+import Loader from '@/components/core/Loader';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <>
       <html lang="en">
@@ -21,12 +23,16 @@ export default function RootLayout({
           <link rel="icon" href="./favicon.ico" />
           <title>SF</title>
         </head>
-          <body className={`${GeistSans.className} px-2 dark mx-auto`}>
+          <body className={`${GeistSans.className} px-2 dark text-white mx-auto`}>
+          <Loader />
               <SmoothScrolling>
-                  {children}
-                  <Footer/>
+                <div className="h-[50rem] w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+                  <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+                    {children}
+                    </div>
               </SmoothScrolling>
               <Analytics debug={false}/>
+           
           </body>
       </html>
     </>
---
+++ components/core/Loader.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+
+const Loader = () => {
+    const [showLoader, setShowLoader] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowLoader(false);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className={`bg-gray-900 text-center fixed top-0 left-0 w-full h-full flex items-center justify-center transition-opacity duration-500 ${showLoader ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+        <div className={`loader transform transition-transform duration-500 ${showLoader ? 'translate-y-0' : '-translate-y-full'}`}>
+            <div className="spinner"></div>
+        </div>
+    </div>
+    );
+};
+
+export default Loader;
