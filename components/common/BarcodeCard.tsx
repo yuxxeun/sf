67a63345dd2648cbd21cd9b9b4f37542d5be40a4
yuxxeun <itsyuxxeun@gmail.com>
2024-03-21 13:56:06 +0700

    fix: qr code padding

--- components/common/BarcodeCard.tsx
+++ components/common/BarcodeCard.tsx
@@ -8,13 +8,13 @@ const BarcodeCard = () => {
     return (
         <>
             <div className={`${GeistMono.className} mt-3 mx-auto border rounded-xl`}>
-                <div className="items-center px-3 py-4 flex justify-center space-x-7">
+                <div className="items-center px-3 py-4 flex justify-center space-x-5">
                     <div>
-                        <QRCode bgColor="#fff" size={120} value={defaultWebsiteData.url}/>
+                        <QRCode bgColor="#fff" size={110} value={defaultWebsiteData.url}/>
                     </div>
                     <div>
-                        <p>
-                            npm i @yuxxeun/sf   
+                        <p className="text-lg">
+                            npm i @yuxxeun/sf
                         </p>
                     </div>
                 </div>
